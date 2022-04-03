9c8c6fb679d995fd8843f71e2eb17372307b74ba
jorge <gorgenavarro3000@gmail.com>
2022-04-03 12:43:02 -0400

    CURSO-JAVASCRIPT

---
+++ 1-tipos-de-operadores/scripts.js
@@ -0,0 +1,46 @@
+/*Operadores Matematicos
+let a = 5;
+let b = 2;
+
+let usuario = 'carlos';
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+console.log('hola' + usuario);*/
+
+/*Operadores Incrementa/Decrementa*/
+/*let a = 5;*/
+
+/*console.log(a);
+console.log(a++);
+console.log(a);
+console.log(++a);
+console.log(a);*/
+
+/*console.log(a--);
+console.log(a);
+console.log(--a);
+console.log(a);*/
+
+/*Operadores Asignacion*/
+
+let a = 10;
+
+a + a+3;
+console.log(a);
+a +=3;
+console.log(a);
+a -=3;
+console.log(a);
+a *=3;
+console.log(a);
+a /=3;
+console.log(a);
+a %=3;
+
+
+
