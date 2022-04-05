b0f080c2fd92f1cd9adbff4eecefe201e3356044
jorge <gorgenavarro3000@gmail.com>
2022-04-04 22:49:36 -0400

    4-condicionales

---
+++ 4-condicionales/script.js
@@ -0,0 +1,26 @@
+/* condicionl simple*/
+let num = 5;
+
+if(num>0){
+    console.log(`$(num) es mayor que 0`);
+    console.log(`$(num) es mayor que 0`);
+}
+
+/*condicional compuesto*/
+if(num>0){
+    console.log(`$(num) es mayor que 0`);
+
+    }else{
+    console.log(`$(num) es menor que 0`);
+}
+
+/*condicionales multiples*/
+
+if(num>0){
+    console.log(`$(num) es mayor que 0`);
+
+}else if(num<0){
+    console.log(`$(num) es menor que 0`);
+}else{
+    console.log(`$(num) es igual a 0`)
+}
