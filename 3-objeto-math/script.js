c3ea010363e103a2d89c7d42049eee81ecaeca1a
jorge <gorgenavarro3000@gmail.com>
2022-04-04 21:26:25 -0400

    3-objeto-math

---
+++ 3-objeto-math/script.js
@@ -0,0 +1,21 @@
+
+console.log(Math.E);
+console.log(Math.PI);
+
+let num = 5.9;
+
+console.log(Math.abs(num));
+
+console.log(Math.ceil(num));
+
+console.log(Math.floor(num));
+
+console.log(Math.pow(3,3))
+
+console.log(Math.round(Math.random()*100));
+
+console.log(Math.round(Math.random() * (100 - 50)+50));
+
+console.log(Math.sign(0));
+
+console.log(Math.sqrt(2));
