00c277ee2b6f519738f5a26ecce273849322aca5
jorge <gorgenavarro3000@gmail.com>
2022-04-06 22:50:16 -0400

    7-operadores-ternarios modificado

--- 7-operadores-ternario/script.js
+++ 7-operadores-ternario/script.js
@@ -10,10 +10,10 @@ else console.log(`$(num) es impar`);*/
 (num % 2 == 0)?
 (
     console.log(`${num} es par`),
-    console.log(`${num} es par 2`);
+    console.log(`${num} es par 2`)
 )
 :
 (
     console.log(`${num} es impar`),
-    console;log(`${num} es impar`)
+    console.log(`${num} es impar`)
 )
