41acffdf81d5902a23fd3484da1aaec11b92b6bf
jorge <gorgenavarro3000@gmail.com>
2022-04-09 10:33:22 -0400

    9-arrays-introduccion

---
+++ 9-arrays-instroduccion/script.js
@@ -0,0 +1,12 @@
+// ARRAYS
+
+let numeros=[1,2,3,4,5];
+
+let palabras=["hola", "estamos", "en", "youtube"];
+
+let booleans=[true,true,false];
+console.log(numeros[0]+numeros[2]);
+
+console.log(`la palabra "${palabras[1]}" tiene ${palabras[1].length} letras `);
+
+console.log(numeros);
