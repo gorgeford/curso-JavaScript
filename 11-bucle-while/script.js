24cd42151386740615f0314ad9cf4ea47d87a178
jorge <gorgenavarro3000@gmail.com>
2022-04-09 13:05:25 -0400

    11-bucle-while

---
+++ 11-bucle-while/script.js
@@ -0,0 +1,16 @@
+// Bucle while
+
+let pass = '';
+
+/*while(pass != 'hola'){
+    pass = prompt('Introduzca su contraseña');
+}
+
+console.log('fin del bucle');*/
+
+//Bucle do while
+
+do{
+    pass = prompt('Introduzca su contraseña');
+
+}while(pass != 'hola')
