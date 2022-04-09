2d515004a6a52629f355bf84844b20e054a7f832
jorge <gorgenavarro3000@gmail.com>
2022-04-09 17:03:30 -0400

    12-bucle-for

---
+++ 12-bucle-for/script.js
@@ -0,0 +1,12 @@
+// bucle For
+
+//su sintaxis se compone de 3 partes iniciacion de variable numeros de vuelta incremento o decremento 
+
+/*for(let i=0;i<=10;i++){
+    codigo a ejecutar
+}*/
+
+let numbers =[56,14,23,37,41,59]
+for(let i=0;i<numbers.length;i++){
+    console.log(`i vale ${i} y el valor deesa psicion en el array es ${numbers[i]}`);
+}
