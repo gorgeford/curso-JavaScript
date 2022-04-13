d55bd1597f07df343bd68590ed44aa118f0e6326
jorge <gorgenavarro3000@gmail.com>
2022-04-12 22:08:59 -0400

    16- funciones-introduccion

---
+++ 16-funciones-introduccin/script.js
@@ -0,0 +1,26 @@
+/* function saludar(){
+    console.log("hola");
+} */
+
+/* const saludar = () => console.log("Hola");
+const saludarusuario = (user) => console.log(`hola ${user}`);
+
+saludarUsuario('Pepe')
+saludarUsuario('Marta')*/
+
+/*
+cons suma = (num1, num2) => {
+    if(num1 == 2){
+        return num1+num2
+    }
+    console.log("estos no se va ejecutar si entra em el if");
+    return num1
+
+}
+console.log(suma(7,3));
+*/
+cons suma = (num1, num2)  => num1+num2
+let result = suma(3,6)
+
+console.log(result);
+
