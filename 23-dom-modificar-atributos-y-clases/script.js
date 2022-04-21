3a5f886defefcbe106110f5da0101d10561e8822
jorge <gorgenavarro3000@gmail.com>
2022-04-20 21:45:30 -0400

    don-modificar-atributos-y-clases

---
+++ 23-dom-modificar-atributos-y-clases/script.js
@@ -0,0 +1,22 @@
+/*
+Atributos
+   element.getAttribute('attribute')
+   element.setAttribute('atribute', value)
+
+Clases
+    element.classList.add('class','class',...)
+    element.classList.remove('class','class',...)
+    element.classList.contains('class')
+    element.classList.replace('oldclass, newClass)
+    element.classList.tpggle('class','class'[,force])
+       
+Atributos directos
+     ide
+     value
+*/
+
+const title = document.getElementById('title')
+const name = document.getElementById('name')
+
+console.log(title)
+console.log(name)
