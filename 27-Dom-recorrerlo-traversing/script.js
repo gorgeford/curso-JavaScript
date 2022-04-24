87d1aa070146f38bbd9a7b5b8520321455400a0b
jorge <gorgenavarro3000@gmail.com>
2022-04-24 10:46:52 -0400

    27-Dom-recorrerlo-traversing

---
+++ 27-Dom-recorrerlo-traversing/script.js
@@ -0,0 +1,16 @@
+const parent = document.getElementById('parent')
+
+//console.log(parent.parentElement)
+
+//console.log(parent.childNodes)
+//console.log(parent.children)
+//console.log(parent.firstChild)
+//console.log(parent.firstElementChild)
+//console.log(parent.lastChild)
+//console.log(parent.lastElementChild)
+//console.log(parent.firstChild.hasChildNodes())
+
+//console.log(parent.nextSibling)
+//console.log(parent.parentElement.nextElementSibling)
+//console.log(parent.parentElement.previousSibling)
+console.log(parent.parentElement.previousElementSibling)
