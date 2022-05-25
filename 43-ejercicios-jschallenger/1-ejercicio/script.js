d7734d4ffdd473ddc46cde88b058e31777ace332
jorge <gorgenavarro3000@gmail.com>
2022-05-24 21:21:05 -0400

    43-ejercicios-jschallenger

---
+++ 43-ejercicios-jschallenger/1-ejercicio/script.js
@@ -0,0 +1,11 @@
+// Write a function that takes two numbers (a and b) as argument
+// Sum a and b
+// Return the result
+
+function myFunction(a, b){
+
+return a+b 
+
+}
+
+console.log(7+7);
---
+++ 43-ejercicios-jschallenger/3-ejercicio/script.js
@@ -0,0 +1,10 @@
+// Write a function that takes a string (a) and a number (n) as argument
+// Return the nth character of 'a'
+
+function mmyFunction(a, n){
+
+return a[n - 1];
+
+}
+
+console.log([5]);
