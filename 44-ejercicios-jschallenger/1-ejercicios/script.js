780fca2ba5299d4822613339f5e10242c95897b9
jorge <gorgenavarro3000@gmail.com>
2022-05-25 21:18:45 -0400

    44-ejercicios-jschallenger

---
+++ 44-ejercicios-jschallenger/1-ejercicios/script.js
@@ -0,0 +1,11 @@
+// Write a function that takes a string (a) as argument
+// Get the first 3 characters of a
+// Return the result
+function myFunction(a){
+
+return a.slice(0,3);
+
+}
+
+console.log(myFunction('12345678',(0,3)));
+
