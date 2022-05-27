1d9c1d26bdc3d78b0d205cb43f9c4d10b3f18522
jorge <gorgenavarro3000@gmail.com>
2022-05-26 22:03:31 -0400

    45-ejercicios-jschallenger

---
+++ 45-ejercicicos-jschallenger/1-ejercicios/script.js
@@ -0,0 +1,10 @@
+// Write a function that takes two numbers (a and b) as argument
+// Return b percent of a
+
+function myFunction(a, b){
+
+return  result = a/ 100 * b
+
+}
+
+console.log(myFunction(500,25));
---
+++ 45-ejercicicos-jschallenger/3-ejercicios/script.js
@@ -0,0 +1,10 @@
+// Write a function that takes a string (a) as argument
+// Remove the last 3 characters of a
+// Return the result
+function myFunction(a){
+
+return a.slice(0,-3);
+
+}
+
+console.log(myFunction('abcdefghij',(0,-3)));
