b4289a8fc720b09bf142699cdf49d0be97286fea
jorge <gorgenavarro3000@gmail.com>
2022-05-30 19:41:23 -0400

    47-ejercicio agregados los dos pendientes

---
+++ 47-ejercicio-dia-6/4-ejerc/script.js
@@ -0,0 +1,11 @@
+// Write a function that takes an array (a) as argument
+// Remove the first 3 elements of 'a'
+// Return the result
+
+function myFunction(a){
+
+return a.slice(3);
+
+}
+
+console.log(myFunction([1,2,3,4,5,6]));
---
+++ 47-ejercicio-dia-6/5-ejerc/script.js
@@ -0,0 +1,12 @@
+//Necesitamos una función que pueda transformar un número en una cadena.
+//¿Qué formas de lograr esto conoces?
+//Ejemplos:
+//123 --> "123"
+//999 --> "999"
+
+function numberToString(num) {
+    
+   return String(num);
+  }
+
+  console.log(numberToString("444"));
