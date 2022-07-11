93f1505632f355f85d950d6d144455fdfd9e6c42
jorge <gorgenavarro3000@gmail.com>
2022-07-11 17:13:18 -0400

    61 ejercicio de prctica

---
+++ 61-ejercicio/script.js
@@ -0,0 +1,37 @@
+//Escribe una función que tome una string (a) como argumento
+// Eliminar los primeros 3 caracteres del string, Devuelve el resultado
+
+
+
+const string = (a) => {
+    
+return a.slice(3);
+
+}
+
+console.log(string('abcdef'));
+
+
+
+
+
+
+
+
+
+
+
+//Escribe una función que tome una string como argumento
+// Extrae los últimos 3 caracteres de la cadena
+// Devuelve el resultado
+
+/*
+function cadena (str){
+
+    return str.slice(-3);
+
+}
+
+console.log(cadena('abcdefg'));
+
+*/
